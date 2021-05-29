5786cdfe021240969e8d534388e4ecb992bbc7bb
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-29 11:50:35 -0300

    Utilizando fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,3 +1,9 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold, useFonts
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
@@ -6,6 +12,16 @@ import { Dashboard } from './src/screens/Dashboard';
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
