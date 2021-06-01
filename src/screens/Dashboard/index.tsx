4b9c54f7aab85904659d52ef1941768aac62cd30
guiialmeiida <gui.almeidacrz@gmail.com>
2021-05-31 21:20:50 -0300

    Utilizando proporções e icones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+    Container,
+    Header,
+    Icon,
+    Photo,
+    User,
+    UserGreeting,
+    UserInfo,
+    UserName,
+    UserWrapper
+} from './styles';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/29558688?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Guilherme</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
