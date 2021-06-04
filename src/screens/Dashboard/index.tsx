6845e20d9e5eea2bf4c52f52961c7ac383ae4e46
guiialmeiida <gui.almeidacrz@gmail.com>
2021-06-04 07:44:15 -0300

    Utilizando listas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 import { HighlighCard } from '../../components/HighlighCard';
 import { TransactionCard } from '../../components/TransactionCard';
@@ -16,9 +17,40 @@ import {
     HighlighCards,
     Transactions,
     Title,
+    TransactionList,
 } from './styles';
 
 export function Dashboard() {
+    const data = [
+        {
+            title: 'Desenvolvimento de site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: '13/04/2021'
+        },
+        {
+            title: 'Desenvolvimento de site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: '13/04/2021'
+        },
+        {
+            title: 'Desenvolvimento de site',
+            amount: 'R$ 12.000,00',
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign'
+            },
+            date: '13/04/2021'
+        },
+    ];
+
     return (
         <Container>
             <Header>
@@ -60,7 +92,15 @@ export function Dashboard() {
             <Transactions>
                 <Title>Listagem</Title>
 
-                <TransactionCard />
+                <TransactionList
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace()
+                    }}
+                />
+
             </Transactions>
         </Container>
     )
