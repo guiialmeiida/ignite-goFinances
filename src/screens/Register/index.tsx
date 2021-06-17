5db52252d2b44e8cb2697b09d230e71c219eb6d4
guiialmeiida <gui.almeidacrz@gmail.com>
2021-06-17 08:15:24 -0300

    Listando transações

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -48,8 +48,6 @@ export function Register() {
     const [transactionType, setTransactionType] = useState('');
     const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
-    const dataKey = '@goFinances:transactions';
-
     const [category, setCategory] = useState({
         key: 'category',
         name: 'Categoria',
@@ -66,7 +64,7 @@ export function Register() {
         resolver: yupResolver(schema)
     });
 
-    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+    function handleTransactionsTypeSelect(type: 'positive' | 'negative') {
         setTransactionType(type);
     }
 
@@ -89,12 +87,14 @@ export function Register() {
             id: String(uuid.v4()),
             name: form.name,
             amount: form.amount,
-            transactionType,
+            type: transactionType,
             category: category.key,
             date: new Date()
         }
 
         try {
+            const dataKey = '@goFinances:transactions';
+
             const data = await AsyncStorage.getItem(dataKey);
             const currentData = data ? JSON.parse(data) : [];
 
@@ -149,15 +149,15 @@ export function Register() {
                             <TransactionTypeButton
                                 type='up'
                                 title='Income'
-                                onPress={() => handleTransactionsTypeSelect('up')}
-                                isActive={transactionType === 'up'}
+                                onPress={() => handleTransactionsTypeSelect('positive')}
+                                isActive={transactionType === 'positive'}
                             />
 
                             <TransactionTypeButton
                                 type='down'
                                 title='Outcome'
-                                onPress={() => handleTransactionsTypeSelect('down')}
-                                isActive={transactionType === 'down'}
+                                onPress={() => handleTransactionsTypeSelect('negative')}
+                                isActive={transactionType === 'negative'}
                             />
                         </TransactionTypes>
 
